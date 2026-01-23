e504baf419201d41e9fc897571cf89dfd9f62733
Kurapika <victorakhihiero@gmail.com>
2026-01-24 00:22:30 +0100

    Update chat.tsx

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -2,7 +2,7 @@ import { Message } from "@/assets/icons";
 
 const Chat = () => {
   return (
-    <button className="clamp-[size,10,12] bg-new-black rounded-full border border-white/20 grid place-items-center fixed clamp-[right,4,20] bottom-[30%] hover:scale-105 transition-transform duration-200">
+    <button className="clamp-[size,10,12] bg-new-black rounded-full border border-white/20 grid place-items-center fixed z-100 clamp-[right,4,20] bottom-[30%] hover:scale-105 transition-transform duration-200">
       <span className="sr-only">Chat</span>
 
       <Message className="clamp-[size,4.5,6] text-white" />
